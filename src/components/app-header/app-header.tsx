fc4dbe6b6c7795bbdfdb5532bba529e5585a4d15
aandreychenko <alexander.andreychenko@gmail.com>
2026-07-05 22:40:22 +0300

    feat [sprint-3]: Оптимизирует работу ссылок в хэдере

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -4,52 +4,46 @@ import {
   Logo,
   ProfileIcon,
 } from '@krgaa/react-developer-burger-ui-components';
-import { useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import styles from './app-header.module.css';
 
 export const AppHeader = (): React.JSX.Element => {
-  const location = useLocation();
-
-  const isActive = (link: string): boolean => {
-    const pathname = location.pathname;
-
-    if (link === '/') {
-      return pathname === '/' || pathname.startsWith('/ingredients/');
-    }
-    if (link === '/profile') {
-      return pathname === '/profile' || pathname.startsWith('/profile/');
-    }
-    return pathname === link;
-  };
-
-  const getLinkClasses = (link: string): string => {
-    return `${styles.link} ${isActive(link) && styles.link_active}`;
-  };
-
   return (
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
         <div className={styles.menu_part_left}>
-          <a href="/" className={getLinkClasses('/')}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `${styles.link} ${isActive && styles.link_active}`
+            }
+          >
             <BurgerIcon type="primary" />
             <p className="text text_type_main-default ml-2">Конструктор</p>
-          </a>
-          <a href="/feed" className={`${getLinkClasses('/feed')} ml-10`}>
+          </NavLink>
+          <NavLink
+            to="/feed"
+            className={({ isActive }) =>
+              `${styles.link} ${isActive && styles.link_active} ml-10`
+            }
+          >
             <ListIcon type="secondary" />
             <p className="text text_type_main-default ml-2">Лента заказов</p>
-          </a>
+          </NavLink>
         </div>
         <div className={styles.logo}>
           <Logo />
         </div>
-        <a
-          href="/profile"
-          className={`${getLinkClasses('/profile')} ${styles.link_position_last}`}
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            `${styles.link} ${isActive && styles.link_active} ${styles.link_position_last}`
+          }
         >
           <ProfileIcon type="secondary" />
           <p className="text text_type_main-default ml-2">Личный кабинет</p>
-        </a>
+        </NavLink>
       </nav>
     </header>
   );
