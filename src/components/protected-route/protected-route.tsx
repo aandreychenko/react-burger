1a89acf32fe8d9720a483b8d38d922cc5106cb07
aandreychenko <alexander.andreychenko@gmail.com>
2026-07-17 15:28:49 +0300

    fix [sprint-4]: Улучшает проверку авторизации

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -3,6 +3,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 
 import { useAppSelector } from '@services/hooks/hooks.ts';
 import { selectIsAuthChecked, selectUser } from '@services/store/user/slice.ts';
+import { isTokenExists } from '@utils/token.ts';
 
 import type React from 'react';
 
@@ -23,19 +24,20 @@ export const ProtectedRoute = ({
 }: ProtectedRouteProps): React.JSX.Element | null => {
   const user = useAppSelector(selectUser);
   const isAuthChecked = useAppSelector(selectIsAuthChecked);
+  const isAuthenticated = !!user && isTokenExists();
   const location = useLocation();
 
   if (!isAuthChecked) {
     return <Preloader />;
   }
 
-  if (onlyUnAuth && user) {
+  if (onlyUnAuth && isAuthenticated) {
     const state = location.state as LocationState;
     const from = state?.from?.pathname ?? '/';
     return <Navigate to={from} replace />;
   }
 
-  if (!onlyUnAuth && !user) {
+  if (!onlyUnAuth && !isAuthenticated) {
     return <Navigate to={'/login'} state={{ from: location }} replace />;
   }
 
